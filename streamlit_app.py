32a02df95afd6797e37094912a4931113254878a
jorgkurzke <Jorgkurzke@gmail.com>
2026-06-11 14:34:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -631,9 +631,14 @@ if uploaded_files:
         # -----------------------------
         # ZUSAMMENFASSUNG
         # -----------------------------
-        st.subheader("📋 Kontroll‑ & Pausentabelle")
-        summary_df = build_summary_table(df)
-        st.dataframe(summary_df)
+       st.subheader("📋 Kontroll‑ & Pausentabelle")
+       summary_df = build_summary_table(
+           df,
+           st.session_state["control_points"],
+           st.session_state["pauses"]
+       )
+
+       st.dataframe(summary_df)
 
         # -----------------------------
         # EXCEL EXPORT
